4e9333fe3a5a4f67085ac200187d9f65d302a5db
Lama Gharib <lamagharib3@gmail.com>
2021-10-21 11:20:46 +0200

    adding context folder

---
+++ frontend/client/src/context/context.js
@@ -0,0 +1,32 @@
+import React, { createContext, useState, useEffect } from 'react';
+export const MenuContext = createContext();
+
+export const MenuProvider = ({ children }) => {
+  const [menu, setMenu] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+  const data = async () => {
+    try {
+      setIsLoading(true);
+      const result = await fetch('http://192.168.2.17:5000/api/menu');
+      const jsonData = await result.json();
+      setMenu(jsonData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    data();
+  }, []);
+
+  return (
+    <MenuContext.Provider
+      value={{ menu, cart, setCart, isLoading, error, total, setTotal }}
+    >
+      {children}
+    </MenuContext.Provider>
+  );
+};
