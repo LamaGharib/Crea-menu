783437f128ff649be4edbb80bd64121d6451093f
Lama Gharib <lamagharib3@gmail.com>
2021-12-27 13:06:20 +0100

    fixing the menu items

--- backend/index.js
+++ backend/index.js
@@ -430,33 +430,20 @@ app.get("/api/menu", (req, res) => {
             {
               id: uuidv4(),
               name: "chicken nuggets (7 pcs)",
-              price: "",
+              price: "5.90",
             },
             {
               id: uuidv4(),
               name: "mix snacks small (12 pcs)",
               price: "10",
             },
-            {
-              id: uuidv4(),
-              name: "chicken nuggets (7 pcs)",
-              price: "",
-            },
+
             {
               id: uuidv4(),
               name: "mix snacks big (20 pcs)",
               price: "16",
             },
-            {
-              id: uuidv4(),
-              name: "chicken nuggets (7 pcs)",
-              price: "",
-            },
-            {
-              id: uuidv4(),
-              name: "mix snacks small (12 pcs)",
-              price: "10",
-            },
+
             { id: uuidv4(), name: "mixed nuts", price: "2.80" },
             {
               id: uuidv4(),
